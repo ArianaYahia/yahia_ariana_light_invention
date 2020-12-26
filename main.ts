deb3e87ccb53e63e4ff82caaa3d67243332de742
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2020-12-26 13:43:40 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+while (true) {
+    console.log("Light Level:" + input.lightLevel())
+    if (input.lightLevel() > 6) {
+        light.clear()
+    } else {
+        light.setAll(light.rgb(255, 255, 255))
+    }
+    
+}
